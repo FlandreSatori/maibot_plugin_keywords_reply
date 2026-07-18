4301a77e00724a4a22e777c35d6e3157cb1d2e1f
FlandreSatori <satori@mail.ustc.edu.cn>
2026-07-18 16:59:05 +0800

    更换默认密码

--- editor.bat
+++ editor.bat
@@ -1,3 +1,3 @@
 @echo off
-REM 可选：在末尾加 --token 你的密码，然后用 http://127.0.0.1:8765/?token=你的密码 访问
-python .\editor\server.py --data-dir ..\..\data\plugins\maibot_plugin.keywords_reply --port 8765 --token 123
+REM 可选参数：--token 密码
+python .\editor\server.py --data-dir ..\..\data\plugins\maibot_plugin.keywords_reply --port 8765 --token 1are2you3winning4son5
